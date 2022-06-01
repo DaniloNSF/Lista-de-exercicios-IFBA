57b783830a06607090296353bf220f32ee96eb11
Danilo Nascimento dos Santos Ferreira <danilo.nascimento@ebserh.gov.br>
2022-06-01 11:46:04 -0300

    Exercicios

--- Questao03.js
+++ Questao03.js
@@ -1,14 +1,14 @@
            
-var
+let raio, area, perimetro;
     
-    raio, area, perimetro:inteiro        
-
-inicio
-
-    raio <- 2
-    area <- 3,14 * (2+2)
-    perimetro <- 2 * 3,14 * 2
-    escreva ("A area da circunferência é = " , area) 
-    escreva ("O perimetro da circunferência é = " , perimetro)      
+    raio =  2
+    area =  3,14 * (2+2)
+    perimetro =  2 * 3,14 * 2
+    console.log ("A area da circunferência é = " , area) 
+    console.log ("O perimetro da circunferência é = " , perimetro)      
    
-fimalgoritmo
+       
+
+
+
+    
