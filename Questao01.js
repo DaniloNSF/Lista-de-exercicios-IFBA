521470c38ca75f7b257f6c42daaca89f178111ef
Danilo Nascimento dos Santos Ferreira <danilo.nascimento@ebserh.gov.br>
2022-06-01 11:37:20 -0300

    Exercicios

--- Questao01.js
+++ Questao01.js
@@ -1,18 +1,14 @@
 
-var
+let base, altura, area, perimetro;
     
-    base, altura, area, perimetro:inteiro
+    base = 6
+    altura = 2
+    area = base * altura  
+    perimetro =  (2*2) + (2*6)
+    console.log ("A area do retangulo é ", area)
+    console.log ("o perimetro do retangulo é ", perimetro)
 
-inicio 
-    
-    base <- 6
-    altura <- 2
-    area <- base * altura  
-    perimetro <- (2*2) + (2*6)
-    escreva ("A area do retangulo é ", area)
-    escreva ("o perimetro do retangulo é ", perimetro)
 
-fimalgoritmo
 
 
 
