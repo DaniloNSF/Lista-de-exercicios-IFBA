30fcf19e20cb5feb95b032bd8b24fb056a218aae
Danilo Nascimento dos Santos Ferreira <danilo.nascimento@ebserh.gov.br>
2022-06-01 11:41:02 -0300

    Exercicios

--- Questao02.js
+++ Questao02.js
@@ -1,15 +1,15 @@
 
-var
+let area, base, altura, perimetro;
 
-    area, base, altura, perimetro:inteiro  
+     base = 4;
+    altura =  4;
+    area =  base * altura 
+    perimetro =  4 + 4 + 4 + 4
+    console.log ("A area do quadrado é = " , area) 
+    console.log ("O perimetro do quadrado é = " , perimetro)
+    
 
-inicio
+
     
-    base <- 4
-    altura <- 4
-    area <- base * altura 
-    perimetro <- 4 + 4 + 4 + 4
-    escreva ("A area do quadrado é = " , area) 
-    escreva ("O perimetro do quadrado é = " , perimetro)
     
-fimalgortimo
+
